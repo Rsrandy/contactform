54bc3b50ab80de39295d254030fd545796ffb6bc
Randhir singh <46832545+Rsrandy@users.noreply.github.com>
2022-09-19 13:14:39 +0530

    Update message.php

--- message.php
+++ message.php
@@ -1,4 +1,3 @@
-//Contact Form in PHP
 <?php
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
@@ -8,19 +7,18 @@
 
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "rsrandy68@gmail.com"; //enter that email address where you want to receive all messages
+      $receiver = "contact@rmgtech.in"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
-      $sender = "From: $email";
+      $sender = "From: $email";}
       if(mail($receiver, $subject, $body, $sender)){
-         echo "Your message has been sent";
-      }else{
-         echo "Sorry, failed to send your message!";
-      }
-    }else{
-      echo "Enter a valid email address!";
-    }
-  }else{
+         echo "Your message has been sent";}
+      else{
+         echo "Sorry, failed to send your message!";}
+     else{
+      echo "Enter a valid email address!";}
+  else{
     echo "Email and message field is required!";
   }
+}
 ?>
